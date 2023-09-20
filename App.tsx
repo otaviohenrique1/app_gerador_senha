68371edc6418ecd49f5d43b6e538a3872f43056a
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-20 13:33:24 -0300

    commit 3

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import GeradorSenha from './src/GerenciadorSenha';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GeradorSenha />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
